01bcd148f8c90b543eafb073c3c89fcdfa94045c
Tudor <tudorluchy@gmail.com>
2013-04-23 17:10:40 +0200

    upload

---
+++ selection_exercices.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<script type="text/javascript" src="js/kinetic.js"></script>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/sy08.js"></script>
+		<script type="text/javascript" src="js/truncatable/jquery.truncatable.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script language="JavaScript" type="text/javascript">
+			 $(function(){
+				$('.exo_enonce').truncatable({
+	 								limit:400,
+	 								more: '.....',
+	 								less: true,
+	 								hideText: '[cacher]'
+	 							});
+			})
+		</script>
+	</head>
+	<body>		
+	<div id="corps_selection">
+		<h1 id="titre_exos">Exercices</h1>
+		Sur cette page vous ai présenté un ensemble d'exercices.
+		<h3>Les réseaux de Pétri</h3>
+		<div class="exo_selection">
+			<span class="exo_titre_bis">Exercice 1 : Intitulé de l'exercice</span><br />
+			<span class="exo_enonce">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</span>
+			<a href="#>"><img class="start" src='img/go.png' title='Start exercice!'/></a>
+			<br />
+			<b>Niveau de difficulté : ++</b>
+		</div>
+		<div class="exo_selection">
+			<span class="exo_titre_bis">Exercice 2 : Intitulé de l'exercice</span><br />
+			<span class="exo_enonce">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</span>
+			<a href="#>"><img class="start" src='img/go.png' title='Start exercice!'/></a>
+			<br />
+			<b>Niveau de difficulté : +++</b>
+		</div>
+	</div>	
+	</body>
+</html>
