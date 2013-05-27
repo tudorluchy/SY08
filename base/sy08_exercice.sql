bb828a87b85db4ea9b21f4f1684e733ac277ff0e
Tudor <tudorluchy@gmail.com>
2013-05-27 11:17:17 +0200

    table

--- base/sy08_exercice.sql
+++ base/sy08_exercice.sql
@@ -8,10 +8,13 @@ USE `sy08` ;
 -- -----------------------------------------------------
 -- Table `sy08`.`sy08_exercice`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `sy08`.`sy08_exercice` ;
+
 CREATE  TABLE IF NOT EXISTS `sy08`.`sy08_exercice` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `intitule` VARCHAR(255) NOT NULL ,
   `enonce` LONGTEXT NOT NULL ,
+  `actif` TINYINT(1) NOT NULL ,
   `image` VARCHAR(255) NULL ,
   `fichier` VARCHAR(255) NULL ,
   `difficulte` ENUM('+','++','+++') NOT NULL ,
--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <br />
 <a href="administration.php">Administration : Ajout et Edition des exercices</a></br></br>
 <a href="selection_exercices.php">Selection d'un exercice</a>
+
